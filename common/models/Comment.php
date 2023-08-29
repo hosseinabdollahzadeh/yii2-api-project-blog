b6f3994306c7cb9928bc31cc4a08dc94e49c8228
hossein <abdollahzadeh.hossein@gmail.com>
2023-08-29 06:06:57 +0330

    Create models of comment & post

---
+++ common/models/Comment.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%comment}}".
+ *
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $body
+ * @property int|null $post_id
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property int|null $created_by
+ *
+ * @property User $createdBy
+ * @property Post $post
+ */
+class Comment extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%comment}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['body'], 'string'],
+            [['post_id', 'created_at', 'updated_at', 'created_by'], 'integer'],
+            [['title'], 'string', 'max' => 512],
+            [['post_id'], 'exist', 'skipOnError' => true, 'targetClass' => Post::class, 'targetAttribute' => ['post_id' => 'id']],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'body' => 'Body',
+            'post_id' => 'Post ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'created_by' => 'Created By',
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'created_by']);
+    }
+
+    /**
+     * Gets query for [[Post]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\PostQuery
+     */
+    public function getPost()
+    {
+        return $this->hasOne(Post::class, ['id' => 'post_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\CommentQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\CommentQuery(get_called_class());
+    }
+}
---
+++ common/models/Post.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%post}}".
+ *
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $body
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property int|null $created_by
+ *
+ * @property Comment[] $comments
+ * @property User $createdBy
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%post}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['body'], 'string'],
+            [['created_at', 'updated_at', 'created_by'], 'integer'],
+            [['title'], 'string', 'max' => 512],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'body' => 'Body',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'created_by' => 'Created By',
+        ];
+    }
+
+    /**
+     * Gets query for [[Comments]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\CommentQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::class, ['post_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|\common\models\query\UserQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'created_by']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\PostQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\PostQuery(get_called_class());
+    }
+}
